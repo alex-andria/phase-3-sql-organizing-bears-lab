0d796b9ea6daef11a1b186f3006be8199732a8f8
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-01-14 03:19:16 -0500

    commit tree 2c3a0cdedb33b1123a9ab979347696d88e7b218f

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'kind', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 8, 'F', 'yellow', 'energetic', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 2, 'F', 'orange', 'happy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 4, 'F', 'yellow', 'energetic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 1, 'F', 'black', 'timid', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 8, 'M', 'yellow', 'skittish', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 13, 'F', 'green', 'docile', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 8, 'F', 'yellow', 'energetic', 0);
